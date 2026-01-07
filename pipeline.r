caa234a64fe957df40081ee34de4121e1e55f289
Splayfern3 <splayfern3@gmail.com>
2026-01-06 21:18:01 -0600

    Add loader pipeline and ignore data

--- pipeline.r
+++ pipeline.r
@@ -1,5 +1,36 @@
 library(tidyverse)
-library(GEOquery)
-library(dplyr)
-library(affy)
+library(limma)
+library(sva)
+library(umap)
+library(cluster)
+library(msigdbr)
+library(fgsea)
+
+GSE73461_raw <- read.table(
+  "transcriptome_data/uncompressed/GSE73461_GEOupload_Discovery_Dataset_Normalised_Sept_15_n_459.txt",
+  header = TRUE,
+  sep = "\t",
+  stringsAsFactors = FALSE,
+  check.names = FALSE
+)
+
+cn <- colnames(GSE73461_raw)
+is_id_col <- (cn == "ARRAY_ID")
+is_pval_col <- grepl("Detection", cn)
+is_expr_col <- !is_id_col & !is_pval_col
+
+probe_ids <- GSE73461_raw$ARRAY_ID
+expr_df <- GSE73461_raw[, is_expr_col, drop = FALSE]
+pval_df <- GSE73461_raw[, is_pval_col, drop = FALSE]
+GSE73461_expr <- as.matrix(expr_df)
+GSE73461_pval <- as.matrix(pval_df)
+rownames(GSE73461_expr) <- probe_ids
+rownames(GSE73461_pval) <- probe_ids
+
+pval_names_stripped <- sub("_Detection_Pval$", "", colnames(GSE73461_pval))
+stopifnot(identical(colnames(GSE73461_expr), pval_names_stripped))
+dim(GSE73461_expr)
+dim(GSE73461_pval)
+range(GSE73461_expr, na.rm = TRUE)
+range(GSE73461_pval, na.rm = TRUE)
 
